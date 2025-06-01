7353e6f7baaab60b6a7896e1cf94eb1f6bdd48fc
Gabe Montague <montaguegabe@users.noreply.github.com>
2025-06-01 13:14:27 -0400

    Add REDIS

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REDIS_URL = "redis://localhost:6379/0"
+
+BROKER_URL = REDIS_URL
+
+
